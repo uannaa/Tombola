bcd0f8648ee6d6aa0ba80eefe7dce7781c746f97
uannaa <teermqq@gmail.com>
2024-03-27 19:39:16 +0100

    First commit

--- src/tombola/Tombola.java
+++ src/tombola/Tombola.java
@@ -17,7 +17,72 @@ public class Tombola {
      */
     public static void main(String[] args) {
         
+        int cartella[][] = new int[3][9];
+        int tombolone[][] = new int[9][10];
+        int k = 1;
         
+        
+        //Tombolone
+        for (int i = 0; i < tombolone.length; i++) {
+            
+            for (int j = 0; j < tombolone[i].length; j++) {
+                
+                tombolone[i][j] = k;
+                k++;
+                
+            }
+            
+        }
+        
+        System.out.println("Tombolone: \n");
+        
+        for (int i = 0; i < tombolone.length; i ++) {
+            
+            for (int j = 0; j < tombolone[i].length; j++) {
+                
+                if (tombolone[i][j] < 10) {
+                    
+                    System.out.print("0");
+                    
+                }
+                
+
+                
+                System.out.print(tombolone[i][j] + " ");
+                
+                if(tombolone[i][j] % 10 == 5) {
+                    
+                    System.out.print("| ");
+                    
+                }
+                
+                if (j == 9) {
+                    
+                    System.out.println();
+                    
+                }
+                
+            }
+            
+        }
+        
+        //creazione cartella
+        
+        Random rand = new Random();
+        
+        
+        
+        
+        //print cartella
+        for (int i = 0; i < cartella.length; i++) {
+            
+            for (int j = 0; j < cartella[i].length; j++) {
+                
+                System.out.println(cartella[i][j]);
+                
+            }
+            
+        }
         
     }
     
