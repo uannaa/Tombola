25da3159719df3c5694649316861ada3e254af1d
uannaa <teermqq@gmail.com>
2024-03-28 15:52:56 +0100

    DOESNT WORKING

--- src/tombola/Tombola.java
+++ src/tombola/Tombola.java
@@ -17,6 +17,10 @@ public class Tombola {
      */
     public static void main(String[] args) {
         
+        
+        
+        spaziVuoti();
+        
         int cartella[][] = new int[3][9];
         int tombolone[][] = new int[9][10];
         int k = 1;
@@ -70,21 +74,39 @@ public class Tombola {
         
         Random rand = new Random();
         
-        int max = 9;
-        int min = 1;
+        int max = 0;
+        int min = 0;
         
         for (int i = 0; i < cartella.length; i++) {
             
+            max = 9;
+            min = 0;
+            
             for (int j = 0; j < cartella[i].length; j++) {
-//                
-
                 
+                cartella[i][j] = rand.nextInt(max - min) + min;
+                rimuoviDuplicati(cartella, max, min);
+                
+                
+                max += 10;
+                min += 10;
                 
             }
             
         }
         
+        
+        
         System.out.println("\nCartella: \n");
+        
+        ordinaMatrice(cartella);
+        
+        
+        for (int i = 0; i < 4; i++) {
+            
+            System.out.println(spaziVuoti());
+            
+        }
         
         //print cartella
         for (int i = 0; i < cartella.length; i++) {
@@ -110,6 +132,123 @@ public class Tombola {
             }
             
         }
+        
+        for (int i = 0; i < cartella.length; i++) {
+            
+            for (int j = 0; j < cartella[i].length; j++) {
+                
+                if (cartella[i][j] == 0) {
+                    
+                    
+                    
+                }
+            }
+            
+        }
+        
+
+        
+        System.out.println("\nCartella: \n");
+//        
+//        for (int i = 0; i < cartella.length; i++) {
+//            
+//            for (int j = 0; j < cartella[i].length; j++) {
+//                
+//                if (cartella[i][j] < 10) {
+//                    
+//                    System.out.print("0");
+//                    
+//                }
+//                
+//                System.out.print(cartella[i][j] + " ");
+//                
+//                
+//                
+//                if (j == 8) {
+//                    
+//                    System.out.println();
+//                    
+//                }
+//                
+//            }
+//            
+//        }
+        
+    }
+    
+    public static void rimuoviDuplicati(int matrice[][], int max, int min) {
+        
+        Random rand = new Random();
+        
+        for (int colonna = 0; colonna < matrice[0].length; colonna++) {
+            
+            for (int riga = 0; riga < matrice.length; riga++) {
+                
+                int valoreCorrente = matrice[riga][colonna];
+                
+                for (int confrontoRiga = riga + 1; confrontoRiga < matrice.length; confrontoRiga++) {
+                    
+                    if (matrice[confrontoRiga][colonna] == valoreCorrente) {
+                        matrice[confrontoRiga][colonna] = 0; 
+                    }
+                    
+                }
+            }
+        }
+        
+    }
+    
+    public static void ordinaMatrice(int[][]cartella){
+        
+        for (int col = 0; col < cartella[0].length; col++) {
+
+            for (int i = 0; i < cartella.length - 1; i++) {
+
+                int min = i;
+
+                for (int j = i + 1; j < cartella.length; j++) {
+
+                    if (cartella[j][col] < cartella[min][col]) {
+
+                        min = j;
+
+                    }
+                }
+                if (min != i) {
+
+                    int temp = cartella[i][col];
+                    cartella[i][col] = cartella[min][col];
+                    cartella[min][col] = temp;
+
+                }
+            }
+        }
+    }
+    
+    public static int[] spaziVuoti() {
+        
+        Random rand = new Random();
+        
+        int numvuoti[] = new int[4];
+        int numvuoti2[] = new int[4];
+        int numvuoti3[] = new int[4];
+        
+        for (int i = 0; i < numvuoti.length; i++) {
+            
+            numvuoti[i] = rand.nextInt(8-0) + 0;
+            for (int j = 0; j < numvuoti.length; j++) {
+                
+                while (numvuoti[i] == numvuoti[j]) {
+                    
+                    numvuoti[i] = rand.nextInt(8-0) + 0;
+                    
+                }
+                
+            }
+            
+        }
+        
+        return numvuoti;
         
     }
     
